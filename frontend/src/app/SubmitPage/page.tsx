85b5cb65901fdb956a0431d04ea94c7a946fcb79
travyozza <84102421+travyozza@users.noreply.github.com>
2023-10-03 10:19:42 +1300

    Fixed page layout issues

---
+++ frontend/src/app/SubmitPage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticleSubmitPage: React.FC = () => {
+  return (
+    <div>
+      <h1>SUBMIT PAGE</h1>
+    </div>
+  );
+};
+
+export default ArticleSubmitPage;
