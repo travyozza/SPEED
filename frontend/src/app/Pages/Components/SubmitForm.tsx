d46c5969ac31e7329b74b59c760152be1a7d111b
travyozza <84102421+travyozza@users.noreply.github.com>
2023-10-11 12:18:54 +1300

    Run Build working

--- frontend/src/app/Pages/Components/SubmitForm.tsx
+++ frontend/src/app/Pages/Components/SubmitForm.tsx
@@ -1,70 +1,78 @@
 "use client";
+
 import React, { useState } from 'react';
 
-const SubmitForm = ({ onSubmit }) => {
-  const [formData, setFormData] = useState({
-    title: '',
-    authors: '',
-    journalName: '',
-    year: '',
-    volume: '',
-    number: '',
-    pages: '',
-    doi: '',
-  });
+interface FormValues {
+  title: string;
+  authors: string;
+  journalName: string;
+  year: number;
+  volume: number;
+  number: number;
+  pages: string;
+  doi: string;
+}
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+const initialFormValues: FormValues = {
+  title: '',
+  authors: '',
+  journalName: '',
+  year: 0,
+  volume: 0,
+  number: 0,
+  pages: '',
+  doi: '',
+};
+
+const SubmitForm: React.FC = () => {
+  const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Call the onSubmit prop with form data
-    onSubmit(formData);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formValues); // Replace with your submit logic
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title:</label>
-          <input type="text" name="title" value={formData.title} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Authors:</label>
-          <input type="text" name="authors" value={formData.authors} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Journal Name:</label>
-          <input type="text" name="journalName" value={formData.journalName} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Year of Publication:</label>
-          <input type="text" name="year" value={formData.year} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Volume:</label>
-          <input type="text" name="volume" value={formData.volume} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Number:</label>
-          <input type="text" name="number" value={formData.number} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Pages:</label>
-          <input type="text" name="pages" value={formData.pages} onChange={handleChange} />
-        </div>
-        <div>
-          <label>DOI:</label>
-          <input type="text" name="doi" value={formData.doi} onChange={handleChange} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input type="text" name="title" value={formValues.title} onChange={handleChange} />
+      </label>
+      <label>
+        Authors:
+        <input type="text" name="authors" value={formValues.authors} onChange={handleChange} />
+      </label>
+      <label>
+        Journal Name:
+        <input type="text" name="journalName" value={formValues.journalName} onChange={handleChange} />
+      </label>
+      <label>
+        Year:
+        <input type="number" name="year" value={formValues.year} onChange={handleChange} />
+      </label>
+      <label>
+        Volume:
+        <input type="number" name="volume" value={formValues.volume} onChange={handleChange} />
+      </label>
+      <label>
+        Number:
+        <input type="number" name="number" value={formValues.number} onChange={handleChange} />
+      </label>
+      <label>
+        Pages:
+        <input type="text" name="pages" value={formValues.pages} onChange={handleChange} />
+      </label>
+      <label>
+        DOI:
+        <input type="text" name="doi" value={formValues.doi} onChange={handleChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
