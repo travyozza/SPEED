c88275fb1bff31e0314e02f18f0af9fef088de60
travyozza <84102421+travyozza@users.noreply.github.com>
2023-10-18 15:15:48 +1300

    modertation commit

---
+++ frontend/src/app/Pages/Moderator/page.tsx
@@ -0,0 +1,12 @@
+import AllArticles from '../Components/ModeratorQueue';
+
+const Moderator = () => {
+    return (
+        <div>
+            <h1>Moderation</h1>
+            <AllArticles />
+        </div>
+    );
+};
+
+export default Moderator;
